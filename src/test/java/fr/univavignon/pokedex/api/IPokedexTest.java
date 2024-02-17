5867fa3c89719248e3da51acfdae1945b04dd51f
WalidMedouaz <145688943+WalidMedouaz@users.noreply.github.com>
2024-02-17 12:42:03 +0100

    implémentation de quelques tests

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -2,6 +2,8 @@ package fr.univavignon.pokedex.api;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+
 import org.mockito.Mockito;
 import java.util.ArrayList;
 
@@ -12,14 +14,49 @@ public class IPokedexTest {
     ArrayList<Pokemon> myArrayList;
 
     @Before
-    public void initTestEnvironment() {
+    public void initTestEnvironment() throws PokedexException {
         myPokedex = Mockito.mock(IPokedex.class);
         myBulbizarre = new Pokemon(0, "Bulbizarre", 126,126,90,613,64,4000,4,56);
         myAquali = new Pokemon(133,"Aquali",186,168,260,2729,202,5000,4,100);
         myArrayList = new ArrayList<Pokemon>();
+
+        myArrayList.add(myBulbizarre);
+        myArrayList.add(myAquali);
+
+        Mockito.when(myPokedex.getPokemons()).thenReturn(myArrayList);
+        Mockito.when(myPokedex.size()).thenReturn(myArrayList.size());
+
+        Mockito.when(myPokedex.addPokemon(any(Pokemon.class))).thenAnswer(input -> {
+            Pokemon inputPokemon = input.getArgument(0);
+            myArrayList.add(inputPokemon);
+            return myArrayList.size() - 1;
+        });
+
+        Mockito.when(myPokedex.getPokemon(any(Integer.class))).thenAnswer(input -> {
+            int index = input.getArgument(0);
+            return myArrayList.get(index);
+        });
+    }
+    @Test
+    public void addPokemonTest() throws PokedexException {
+        int index = myPokedex.addPokemon(myBulbizarre);
+        assertEquals(myBulbizarre, myPokedex.getPokemon(index));
     }
 
+    @Test
+    public void sizeTest() {
+        assertEquals(myPokedex.size(), myArrayList.size());
+    }
+    @Test
+    public void getPokemonTest() throws PokedexException {
+        Pokemon p = myPokedex.getPokemon(0);
+        assertEquals(p, myArrayList.get(0));
+    }
 
-
+    @Test
+    public void getPokemonsTest() throws PokedexException {
+        myPokedex.addPokemon(myBulbizarre);
+        assertEquals(myPokedex.getPokemons(), myArrayList);
+    }
 
 }
--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -1,4 +1,29 @@
 package fr.univavignon.pokedex.api;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
 public class IPokemonFactoryTest {
+
+    IPokemonFactory myPokemonFactory;
+    Pokemon myBulbizarre;
+    Pokemon myAquali;
+    @Before
+    public void initTestEnvironment() {
+        myPokemonFactory = Mockito.mock(IPokemonFactory.class);
+        myBulbizarre = new Pokemon(0, "Bulbizarre", 126,126,90,613,64,4000,4,56);
+        myAquali = new Pokemon(133,"Aquali",186,168,260,2729,202,5000,4,100);
+
+        Mockito.when(myPokemonFactory.createPokemon(0, 613, 64, 4000, 4)).thenReturn(myBulbizarre);
+        Mockito.when(myPokemonFactory.createPokemon(133, 2729, 202, 5000, 4)).thenReturn(myAquali);
+    }
+    @Test
+    public void createPokemonTest() {
+        assertEquals(myPokemonFactory.createPokemon(0, 613, 64, 4000, 4), myBulbizarre);
+        assertEquals(myPokemonFactory.createPokemon(133, 2729, 202, 5000, 4), myAquali);
+    }
+
 }

baf90357bc7d6d74c3cb7782bf752d06eb2fcd1d
WalidMedouaz <145688943+WalidMedouaz@users.noreply.github.com>
2024-02-17 13:57:47 +0100

    implémentation de PokedexExceptionTest

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,4 +1,41 @@
 package fr.univavignon.pokedex.api;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.mockito.ArgumentMatchers.any;
+
 public class IPokemonMetadataProviderTest {
+
+    IPokemonMetadataProvider myDataProvider;
+    PokemonMetadata bulbizarreMetadata;
+    PokemonMetadata aqualiMetadata;
+    @Before
+    public void initTestEnvironment() throws PokedexException {
+        myDataProvider = Mockito.mock(IPokemonMetadataProvider.class);
+
+        bulbizarreMetadata = new PokemonMetadata(0, "Bulbizarre", 126, 126,90);
+        aqualiMetadata = new PokemonMetadata(133,"Aquali",186,168,260);
+
+        Mockito.when(myDataProvider.getPokemonMetadata(any(Integer.class))).thenAnswer(input -> {
+            int inputMetadata = input.getArgument(0);
+            if(inputMetadata == 0) {
+                return bulbizarreMetadata;
+            }
+            else if(inputMetadata == 133){
+                return aqualiMetadata;
+            }
+            else {
+                throw new PokedexException("Index inexistant !");
+            }
+        });
+    }
+
+    @Test
+    public void PokedexExceptionTest() throws PokedexException {
+        assertThrows(PokedexException.class, () -> myDataProvider.getPokemonMetadata(126));
+    }
 }
